d49f63224bc7151bc58dfe85e99081b25f5318a1
NicolaPlacentino <nyko0921@gmail.com>
2023-03-16 15:34:52 +0100

    added route api show

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('projects', ProjectController::class);
+Route::get('/projects', [ProjectController::class, 'index']);
+
+Route::get('/projects/{id}', [ProjectController::class, 'show']);
